91179fbc126ae8da3e70272a52b4678b64a1e9ed
GiannisKav7 <58071478+GiannisKav7@users.noreply.github.com>
2024-06-09 21:09:09 +0300

    next version

--- src/Supporters.js
+++ src/Supporters.js
@@ -4,17 +4,17 @@ import "./styles.css";
 function Supporters() {
   return (
     <section id="supporters" className="container my-5">
-      <h2 className="text-center">Οι Υποστηρικτές μας</h2>
+      <h2 className="text-center">Υποστηρικτές</h2>
       <br></br>
       <div className="row">
         <div className="col-md-6 text-center">
-          <a href="https://startsmartsee.org/" target="_blank" rel="noopener noreferrer">
-            <img src="/images/startsmart.jpg" alt="Supporter 1" className="img-fluid supporter-img mb-3" />
+          <a href="https://acein.aueb.gr/en/" target="_blank" rel="noopener noreferrer">
+            <img src="/images/acein.jpg" alt="ACEIn" className="img-fluid uniform-size-img mb-3" />
           </a>
         </div>
         <div className="col-md-6 text-center">
-          <a href="https://acein.aueb.gr/en/" target="_blank" rel="noopener noreferrer">
-            <img src="/images/acein.jpg" alt="Supporter 2" className="img-fluid supporter-img mb-3" />
+          <a href="https://startsmartsee.org/" target="_blank" rel="noopener noreferrer">
+            <img src="/images/startsmart.jpg" alt="StartSmart" className="img-fluid uniform-size-img mb-3" />
           </a>
         </div>
       </div>
